1321516c690c0c5a20dd59afab8bf4c18094fc98
Luis Carranza <carranlu@gmail.com>
2021-03-25 16:16:59 -0600

    Initial Commit

---
+++ StateManagementExample/StateManagementExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateManagementExampleApp.swift
+//  StateManagementExample
+//
+//  Created by Luis Javier Carranza on 25/03/21.
+//
+
+import SwiftUI
+
+@main
+struct StateManagementExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
